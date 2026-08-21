d0fd8c29527dc07526b6d2ef55d37f995ac26ec3
Neha Kananthanam <neha040407@gmail.com>
2026-08-21 10:31:25 +0530

    Time: 301 ms (5.02%), Space: 23.8 MB (82.58%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        int noofint = intervals.size();
+
+        for(int i = 0 ; i < noofint ; i++){
+            for(int j = i ; j < noofint ; j++){
+                if(intervals[i][0] > intervals[j][0]){
+                    swap(intervals[i] , intervals[j]);
+                }
+            }
+        }
+
+        int lower = intervals[0][0] , upper = intervals[0][1];
+
+        for(int i = 1 ; i < noofint ; i++){
+            if(intervals[i][0] <= upper){
+                upper = max(upper , intervals[i][1]);
+            }
+            else{
+                ans.push_back({lower,upper});
+                lower = intervals[i][0];
+                upper = intervals[i][1];
+                i--;
+            }
+        }
+
+        ans.push_back({lower,upper});
+
+        return ans;
+    }
+};
