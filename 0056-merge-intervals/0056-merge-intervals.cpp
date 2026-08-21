736e6eee76cfcaed66aeba63e02d0484929952b9
Neha Kananthanam <neha040407@gmail.com>
2026-08-21 10:32:28 +0530

    Time: 5 ms (51.89%), Space: 23.9 MB (31.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,13 +4,7 @@ public:
         vector<vector<int>> ans;
         int noofint = intervals.size();
 
-        for(int i = 0 ; i < noofint ; i++){
-            for(int j = i ; j < noofint ; j++){
-                if(intervals[i][0] > intervals[j][0]){
-                    swap(intervals[i] , intervals[j]);
-                }
-            }
-        }
+        sort(intervals.begin(), intervals.end());
 
         int lower = intervals[0][0] , upper = intervals[0][1];
 
