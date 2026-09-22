690174d195e97b9d253680df2e0a52ef3d215a4b
Neha Kananthanam <neha040407@gmail.com>
2026-09-22 23:47:45 +0530

    Time: 0 ms (100%), Space: 22.7 MB (77.63%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,32 +11,47 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        stack<ListNode*> st;
         ListNode *slow = head;
         ListNode *fast = head;
-        ListNode *nextNode;
+        ListNode *nextNode = NULL;
+        ListNode *prev = NULL;
 
         if(head == NULL || head->next == NULL)
         return;
 
         while(fast != NULL && fast -> next != NULL){
+            prev = slow;
             slow = slow -> next;
             fast = (fast -> next) -> next;
         }
 
+        prev -> next = NULL;
+
+        prev = NULL;
+
         while(slow != NULL){
-            st.push(slow);
-            slow = slow -> next;
-        }
+            nextNode = slow -> next;
+            slow -> next = prev;
+            prev = slow;
+            slow = nextNode;
+        }        
 
-        while(!st.empty()){
+        fast = prev;
+        nextNode = head;
+
+        while(nextNode != NULL){
+            prev = fast;
             nextNode = head -> next;
-            head -> next = st.top();
-            (head -> next) -> next = nextNode;
+            fast = prev -> next;   //just using fast as a var for the next link in 2nd LL
+            head -> next = prev;
+            prev -> next = nextNode;
             head = (head -> next) -> next;
-            st.pop();
         }
 
-        head -> next = NULL;
+        prev -> next = fast;
+
+        if(fast != NULL){
+            fast -> next = NULL;
+        }
     }
 };
